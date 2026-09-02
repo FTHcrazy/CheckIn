f8556c0424d7976523c5359deadbbad077f2a7d0
方铁汉 <e-tiehan.fang@smart.com>
2026-09-02 16:01:04 +0800

    feat: 美化样式

--- src/components/NavHeader/index.tsx
+++ src/components/NavHeader/index.tsx
@@ -6,15 +6,19 @@ import "./index.scss";
 
 export default function NavHeader() {
   const navigate = useNavigate();
+
   return (
     <div className="nav-header">
-      <Button
-        className="nav-header-icon"
-        onClick={() => navigate(-1)}
-        icon={<ArrowLeftOutlined />}
-      >
-        返回
-      </Button>
+      <div className="nav-header-inner">
+        <Button
+          className="nav-header-icon"
+          type="text"
+          onClick={() => navigate(-1)}
+          icon={<ArrowLeftOutlined />}
+        >
+          返回
+        </Button>
+      </div>
     </div>
   );
 }
